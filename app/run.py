633eae79665d67f710e48009cc05b0e290fb9f05
Minami Ueda <minami.ueda@gmail.com>
2022-03-15 00:19:27 +0900

    Change calculation of fetch date

--- app/run.py
+++ app/run.py
@@ -1,22 +1,25 @@
 import subprocess
 from subprocess import PIPE
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
-fetch_date_dt = datetime.today() - timedelta(days=2)
+# 今月使用分は翌月分として集計される
+# 月末締めだがバッファとして2日前の日付を使用
+fetch_date_dt = datetime.today() + relativedelta(months=1) - timedelta(days=2)
 fetch_date_month = fetch_date_dt.strftime('%Y%m')
 
 print("Running for {}".format(fetch_date_month))
 
-shell_command_paypay = """
-python /app/get-paypay-card-data.py --month {}
-""".format(fetch_date_month)
+# shell_command_paypay = """
+# python /app/get-paypay-card-data.py --month {}
+# """.format(fetch_date_month)
 
-result_paypay = subprocess.run(shell_command_paypay, shell=True, stdout=PIPE, text=True)
-print(result_paypay.stdout)
+# result_paypay = subprocess.run(shell_command_paypay, shell=True, stdout=PIPE, text=True)
+# print(result_paypay.stdout)
 
-shell_command_paypay = """
-python /app/compare.py --month {} --delete-old-file
-""".format(fetch_date_month)
+# shell_command_paypay = """
+# python /app/compare.py --month {} --delete-old-file
+# """.format(fetch_date_month)
 
-result_paypay = subprocess.run(shell_command_paypay, shell=True, stdout=PIPE, text=True)
-print(result_paypay.stdout)
+# result_paypay = subprocess.run(shell_command_paypay, shell=True, stdout=PIPE, text=True)
+# print(result_paypay.stdout)
