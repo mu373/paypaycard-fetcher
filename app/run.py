2c5110133199b4aa00e37c17fa02387b391a007b
Minami Ueda <minami.ueda@gmail.com>
2022-03-27 02:12:09 +0900

    Deprecate run.py

--- app/run.py
+++ app/run.py
@@ -1,25 +1,16 @@
+# NOTICE
+# This script is deprecated. Please use /app/run.sh instead.
+# For migration reason, this script calls run.sh using subprocess.
+
 import subprocess
 from subprocess import PIPE
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 
-# 今月使用分は翌月分として集計される
-# 月末締めだがバッファとして2日前の日付を使用
-fetch_date_dt = datetime.today() + relativedelta(months=1) - timedelta(days=2)
-fetch_date_month = fetch_date_dt.strftime('%Y%m')
+print("Notice: This script (/app/run.py) is deprecated.")
+print("Please use /app/run.sh instead.")
 
-print("Running for {}".format(fetch_date_month))
+shell_command = """
+bash run.sh
+"""
 
-shell_command_paypay = """
-python /app/get-paypay-card-data.py --month {}
-""".format(fetch_date_month)
-
-result_paypay = subprocess.run(shell_command_paypay, shell=True, stdout=PIPE, text=True)
-print(result_paypay.stdout)
-
-shell_command_compare = """
-python /app/compare.py --month {} --delete-old-file --slack --add-category
-""".format(fetch_date_month)
-
-result_compare = subprocess.run(shell_command_compare, shell=True, stdout=PIPE, text=True)
-print(result_compare.stdout)
+result = subprocess.run(shell_command, shell=True, stdout=PIPE, text=True)
+print(result.stdout)
